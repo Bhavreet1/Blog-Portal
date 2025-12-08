58f226b2082febd05fdac1284ebd00a563be5663
Bhavreet Singh <bhavreet24singh@gmail.com>
2025-12-08 23:55:35 +0530

    backend models

---
+++ src/models/post.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+module.exports = mongoose.model("post", postSchema);
